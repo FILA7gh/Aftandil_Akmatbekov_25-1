927b29170ea7571d6a3eda44db19db02757a4895
Aftandil <akmatbekovvv@gmail.com>
2023-02-28 19:14:06 +0600

    added users app

--- Store/urls.py
+++ Store/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from Products.views import main_view, products_view, hashtags_view, product_detail_view, create_product_view
 from django.conf.urls.static import static
 from Store.settings import MEDIA_ROOT, MEDIA_URL
+from users.views import register_view, login_view, logout_view
 
 
 urlpatterns = [
@@ -10,8 +11,14 @@ urlpatterns = [
     path('', main_view),
     path('products/', products_view),
     path('products/<int:id>/', product_detail_view),
+
     path('hashtags/', hashtags_view),
-    path('products/create/', create_product_view)
+
+    path('products/create/', create_product_view),
+
+    path('users/register/', register_view),
+    path('users/login/', login_view),
+    path('user/logout/', logout_view)
 
 ]
 
